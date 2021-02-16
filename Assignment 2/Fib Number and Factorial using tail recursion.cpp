879cf0c8d51d321c60d7ed351d9be2b73081829e
Jayesh Jadhav <43574791+myselfjj@users.noreply.github.com>
2021-02-16 22:59:55 +0530

    Update Fib Number and Factorial using tail recursion.cpp

--- Assignment 2/Fib Number and Factorial using tail recursion.cpp
+++ Assignment 2/Fib Number and Factorial using tail recursion.cpp
@@ -1,1 +1,13 @@
+#include<bits/stdc++.h>
+int fib(int n) {
+double phi = (1 + sqrt(5)) / 2;
+return round(pow(phi, n) / sqrt(5));
+}
+int main ()
+{
+int n = 7;
+std::cout << fib(n) << std::endl;
+return 0;
+}
+
 
