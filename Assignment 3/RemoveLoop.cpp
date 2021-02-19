7858fa58017135c784ae1bdbdb5be247a15f6a8d
Jayesh Jadhav <43574791+myselfjj@users.noreply.github.com>
2021-02-19 11:40:10 +0530

    Add files via upload

---
+++ Assignment 3/RemoveLoop.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int key;
+	struct Node* next;
+};
+
+Node* newNode(int key)
+{
+	Node* temp = new Node;
+	temp->key = key;
+	temp->next = NULL;
+	return temp;
+}
+void printList(Node* head)
+{
+	while (head != NULL) {
+		cout << head->key << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
+void hashAndRemove(Node* head)
+{
+	unordered_map<Node*, int> node_map;
+	Node* last = NULL;
+	while (head != NULL) {
+		if (node_map.find(head) == node_map.end()) {
+			node_map[head]++;
+			last = head;
+			head = head->next;
+		}
+		else {
+			last->next = NULL;
+			break;
+		}
+	}
+}
+int main()
+{
+	Node* head = newNode(50);
+	head->next = head;
+	head->next = newNode(20);
+	head->next->next = newNode(15);
+	head->next->next->next = newNode(4);
+	head->next->next->next->next = newNode(10);
+	head->next->next->next->next->next = head->next->next;
+	hashAndRemove(head);
+
+	printf("Linked List after removing loop \n");
+	printList(head);
+
+	return 0;
+}
